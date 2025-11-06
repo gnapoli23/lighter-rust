6f74462f4da6159aeb3c44ce1bc44ec758a17a8d
yongkangc <chiayongkang@hotmail.com>
2025-11-06 00:34:07 +0000

    chore: remove unused PublicPoolsError type

--- src/apis/account_api.rs
+++ src/apis/account_api.rs
@@ -93,14 +93,6 @@ pub enum PositionFundingError {
     UnknownValue(serde_json::Value),
 }
 
-/// struct for typed errors of method [`public_pools`]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum PublicPoolsError {
-    Status400(models::ResultCode),
-    UnknownValue(serde_json::Value),
-}
-
 /// struct for typed errors of method [`public_pools_metadata`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -690,74 +682,6 @@ pub async fn position_funding(
     } else {
         let content = resp.text().await?;
         let entity: Option<PositionFundingError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent {
-            status,
-            content,
-            entity,
-        }))
-    }
-}
-
-/// Get public pools
-pub async fn public_pools(
-    configuration: &configuration::Configuration,
-    index: i64,
-    limit: i64,
-    authorization: Option<&str>,
-    auth: Option<&str>,
-    filter: Option<&str>,
-    account_index: Option<i64>,
-) -> Result<models::PublicPools, Error<PublicPoolsError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_query_index = index;
-    let p_query_limit = limit;
-    let p_header_authorization = authorization;
-    let p_query_auth = auth;
-    let p_query_filter = filter;
-    let p_query_account_index = account_index;
-
-    let uri_str = format!("{}/api/v1/publicPools", configuration.base_path);
-    let mut req_builder = configuration.client.request(reqwest::Method::GET, &uri_str);
-
-    if let Some(ref param_value) = p_query_auth {
-        req_builder = req_builder.query(&[("auth", &param_value.to_string())]);
-    }
-    if let Some(ref param_value) = p_query_filter {
-        req_builder = req_builder.query(&[("filter", &param_value.to_string())]);
-    }
-    req_builder = req_builder.query(&[("index", &p_query_index.to_string())]);
-    req_builder = req_builder.query(&[("limit", &p_query_limit.to_string())]);
-    if let Some(ref param_value) = p_query_account_index {
-        req_builder = req_builder.query(&[("account_index", &param_value.to_string())]);
-    }
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    if let Some(param_value) = p_header_authorization {
-        req_builder = req_builder.header("authorization", param_value.to_string());
-    }
-
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => return Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::PublicPools`"))),
-            ContentType::Unsupported(unknown_type) => return Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::PublicPools`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<PublicPoolsError> = serde_json::from_str(&content).ok();
         Err(Error::ResponseError(ResponseContent {
             status,
             content,
